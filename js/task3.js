fa735c3a5f386ce568c19bfdb93ea011d28f242e
thekostikoff <kostikoff_terrik@mail.ru>
2022-10-17 16:08:15 +0300

    JSTask3: task3 item2

--- js/task3.js
+++ js/task3.js
@@ -4,23 +4,24 @@ for (i = 0; i < 10; i++) {
 
 	generator.push(Math.floor(Math.random() * 2000) - 1000);
 
-	generator.sort(function (a, b) {
-		return a - b;
-	});
+	generator.sort();
 	console.log(generator);
 
 }
 
 
-function bubbleSort(generator) {
-	for (n = 0; n < n.length; n++) {
-		for (i = 0; i < i.length - 1 - n; i++) {
+function bubbleSort() {
+	for (n = 0; n < generator.length; n++) {
+		for (i = 0; i < generator.length - 1 - n; i++) {
 			if (generator[i] > generator[i + 1]) {
 				const buff = generator[i];
 				generator[i] = generator[i + 1];
 				generator[i + 1] = buff;
 			}
 		}
+
 	}
+	console.log(generator);
 }
-console.log(generator)
+
+bubbleSort();
