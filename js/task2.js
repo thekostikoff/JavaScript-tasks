7519de268b8641e67a74258c5d2a36221b4b9915
thekostikoff <kostikoff_terrik@mail.ru>
2022-10-17 13:42:30 +0300

    JS:change task 2

--- js/task2.js
+++ js/task2.js
@@ -1,22 +1,15 @@
 let generator = [];
-function* massive(start = 0, end = 50) {
-	for (let i = start; i < end; i++) {
-		generator++;
-		yield i;
-	}
-}
-let mas = massive(1, 50);
-for (i of mas) {
-	console.log(i);
+
+for (i = 0; i < 20; i++) {
+
+	generator.push(Math.floor(Math.random() * 1000));
+
+	console.log(generator);
 }
 
-const array1 = [];
-function mass() {
-	for (i = array1; i <= 50; i++) {
-		let map1 = array1.map(i);
-	}
-	console.log(map1);
-}
 
-mass();
+
+
+
+
 
