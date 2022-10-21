da3bce3a9b30fcd95de5a5fce3e6aac72d161123
thekostikoff <kostikoff_terrik@mail.ru>
2022-10-21 15:28:58 +0300

    JSTask4:change 2

--- js/task4.js
+++ js/task4.js
@@ -1,30 +1,31 @@
 const massive = [
 	{ gender: "male", birth: 1990, surname: "ivanovski", age: 2254461463 },
 	{ gender: "female", birth: 2001, surname: "pet", age: 232132 },
-	{ gender: "male", birth: 983, surname: "mocart", age: 123214123 }];
+	{ gender: "male", birth: 983, surname: "mocart", age: 123214123 }
+];
 
 console.log(massive);
 
-let masFil = massive.filter(function (e) {
+let arrayFilter = massive.filter(function (e) {
 	return e.age > 232132 && e.surname.length > 5;
-}
-);
+});
 
-console.log(masFil);
+console.log(arrayFilter);
 
-let masFin = massive.find(function (e) {
+let arrayFind = massive.find(function (e) {
 	return e.birth < 2000;
 });
 
-console.log(masFin);
+console.log(arrayFind);
+
+let arrayPush = massive.push({ gender: "male", birth: 56, surname: "socrat", age: 1 });
+
+console.log(arrayPush);
+console.log(massive);
 
 const numbers = [123.65, 65546, 333.3165];
 
-let masRed = numbers.reduce((total, amount) => total - amount);
+let arrayReduce = numbers.reduce((total, amount) => total - amount);
 
-console.log(masRed);
+console.log(arrayReduce);
 
-let masPush = massive.push({ gender: "male", birth: 56, surname: "socrat", age: 1 });
-
-console.log(masPush);
-console.log(massive);
