ce9a25a787a23bf4db0b0ab54741c75645243187
thekostikoff <kostikoff_terrik@mail.ru>
2022-10-27 16:05:12 +0300

    algoritm/1: change finish

--- js/task5.js
+++ js/task5.js
@@ -2,20 +2,37 @@ let someString = "String";
 let someStringOne = "Hello World";
 let someStringTwo = "1234!_ ";
 
+
 console.log(someString.length);
 console.log(someStringOne.length);
 console.log(someStringTwo.length);
 
-for (i = 0; i < someString.length; i++) {
-	console.log(`S${someString[0]}t${someString[1]}r${someString[2]}i${someString[3]}n${someString[4]}g${someString[5]}`)
-	break;
+function DoubleChar() {
+	let word = '';
+	for (i = 0; i < someString.length; i++) {
+		word = word + (someString[i] + someString[i]);
+	}
+	console.log(word);
 }
 
-for (i = 0; i < someStringOne.length; i++) {
-	console.log(`H${someStringOne[0]}e${someStringOne[1]}l${someStringOne[2]}l${someStringOne[3]}o${someStringOne[4]} ${someStringOne[5]}W${someStringOne[6]}o${someStringOne[7]}r${someStringOne[8]}l${someStringOne[9]}d${someStringOne[10]}`)
-	break;
+DoubleChar();
+
+function DoubleCharOne() {
+	let wordOne = '';
+	for (i = 0; i < someStringOne.length; i++) {
+		wordOne = wordOne + (someStringOne[i] + someStringOne[i]);
+	}
+	console.log(wordOne);
 }
-for (i = 0; i < someStringTwo.length; i++) {
-	console.log(`1${someStringTwo[0]}2${someStringTwo[1]}3${someStringTwo[2]}4${someStringTwo[3]}!${someStringTwo[4]}_${someStringTwo[5]} ${someStringTwo[6]}`)
-	break;
+
+DoubleCharOne();
+
+function DoubleCharTwo() {
+	let wordTwo = '';
+	for (i = 0; i < someStringTwo.length; i++) {
+		wordTwo = wordTwo + (someStringTwo[i] + someStringTwo[i]);
+	}
+	console.log(wordTwo);
 }
+
+DoubleCharTwo();
