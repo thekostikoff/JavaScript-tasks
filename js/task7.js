d7a81ead8badce1284443b6d8c26b4a8cbfb1695
thekostikoff <kostikoff_terrik@mail.ru>
2022-11-01 16:27:37 +0300

    algoritm/3: change 2

--- js/task7.js
+++ js/task7.js
@@ -1,15 +1,13 @@
-let arraySome = ["yoda", "best", "has"];
+function makeWord(array) {
 
-function makeWord() {
-
-	let word = '';
-	for (i = 0; i < arraySome.length; i++) {
-		// console.log(arraySome[i]);
-		for (j = 0; j < arraySome[i].length; j++) {
-			word = (arraySome[0][0] + arraySome[i + 1][j + 1] + arraySome[i + 2][j + 2]);
-			console.log(word);
-			return word;
+	for (i = 0; i < array.length; i++) {
+		let n = 0;
+		while (n < array.length) {
+			console.log(array[i][n]);
+			n++;
+			i++
 		}
 	}
 }
-makeWord();
+
+makeWord(["yoda", "best", "has", "hhhh"]);
