7f779ceb4a4e493e1c9bbebdc7ae860cc3733bc3
thekostikoff <kostikoff_terrik@mail.ru>
2022-11-02 09:10:38 +0300

    algoritm/3: finish

--- js/task7.js
+++ js/task7.js
@@ -3,9 +3,11 @@ function makeWord(array) {
 	for (i = 0; i < array.length; i++) {
 		let n = 0;
 		while (n < array.length) {
-			console.log(array[i][n]);
+			let word = array[0][0] + array[i + 1][n + 1] + array[i + 2][n + 2] + array[i + 3][n + 3];
+			console.log(word)
 			n++;
 			i++
+			return word;
 		}
 	}
 }
