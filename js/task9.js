0a601c52b1f0cd946c636a83dd20c882316abf82
thekostikoff <kostikoff_terrik@mail.ru>
2022-11-14 17:55:23 +0300

    algorithm/5:part 2

--- js/task9.js
+++ js/task9.js
@@ -3,9 +3,14 @@ function arraySumOne(array) {
 		let n = 1;
 		while (n <= array.length) {
 			console.log(array[i++] + n++)
+			let word = [array[0] + 1, array[1] + 2, (array[2] + 3), array[3] + 4];
+			word = word.map((number) => (number > 9) ? number = number.slice(0, 1) : number);
+			console.log(word);
 			return array;
 		}
+
 	}
 }
+
 
 arraySumOne([1, 2, 15, 25]); // 2 4 18 29
