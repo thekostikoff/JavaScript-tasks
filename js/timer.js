bd9a15579e4e574c83a0a47f4dc7ff74d07e012c
thekostikoff <kostikoff_terrik@mail.ru>
2023-01-06 15:30:40 +0300

    timer:part 2

--- js/timer.js
+++ js/timer.js
@@ -1,22 +1,48 @@
+const clock = document.getElementById("result");
+let time = 1000;
+
+function music() {
+	let audio = new Audio();
+	audio.src = 'ooo.mp3';
+	audio.autoplay = true;
+}
+function refresh() {
+	console.log(1);
+}
+
 function go() {
 	let hour = Number(document.getElementById("hour").value);
 	let minute = Number(document.getElementById("minute").value);
 	let second = Number(document.getElementById("second").value);
-	const clock = document.getElementById("result");
-
 	let result = hour * 3600 + minute * 60 + second;
 
-	setInterval(updateClocks, 1000);
-
-	function updateClocks() {
+	let interval = setInterval(function updateClocks() {
 		let hours = Math.floor(result / 3600);
 		let minutes = Math.floor((result - (hours * 3600)) / 60);
 		let seconds = result % 60;
-		hours = hours < 10 ? "0" + hours : hours;
-		minutes = minutes < 10 ? "0" + minutes : minutes;
-		seconds = seconds < 10 ? "0" + seconds : seconds;
-		clock.innerHTML = `${hours}:${minutes}:${seconds}`;
-		result--;
+		if (result < 0) {
+			hour = 0;
+			minute = 0;
+			second = 0;
+			music();
+		} else {
+			hours = hours < 10 ? "0" + hours : hours;
+			minutes = minutes < 10 ? "0" + minutes : minutes;
+			seconds = seconds < 10 ? "0" + seconds : seconds;
+			clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+			result--;
+		}
+	}, time);
+
+	if (document.querySelector('.stop').onclick == true) {
+		clearInterval(interval);
 	}
+
 }
+
+
+
+
+
+
 
